80f1f005ccde3e4364ea74de2576650ccdd6cfd5
Krisztian Holub <kholub1989@gmail.com>
2020-09-24 07:27:59 -0400

    project setup

---
+++ index.js
@@ -0,0 +1,25 @@
+import { LEVEL, OBJECT_TYPE } from "./setup";
+
+// DOM elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game Constants
+const POWER_PILL_TIME = 10000; //ms
+const GLOBAL_SPEED = 80; //ms
+
+// Initial setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {}
