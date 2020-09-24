6890101e3eca0109abc2b0b339f8370dbccdae84
Krisztian Holub <kholub1989@gmail.com>
2020-09-24 08:01:23 -0400

    adding GameBoard class

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { LEVEL, OBJECT_TYPE } from "./setup";
+// CLASSES...
+import GameBoard from "./GameBoard";
 
 // DOM elements
 const gameGrid = document.querySelector("#game");
@@ -8,6 +10,7 @@ const startButton = document.querySelector("#start-button");
 // Game Constants
 const POWER_PILL_TIME = 10000; //ms
 const GLOBAL_SPEED = 80; //ms
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
 
 // Initial setup
 let score = 0;
