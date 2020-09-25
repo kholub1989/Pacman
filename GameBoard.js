e4bcbd6e9b855ce8a4764776c25b26ba571f266d
Krisztian Holub <kholub1989@gmail.com>
2020-09-25 07:50:14 -0400

    adding Pacman class

--- GameBoard.js
+++ GameBoard.js
@@ -39,9 +39,9 @@ class GameBoard {
     this.grid[pos].classList.remove(...classes);
   }
 
-  objectExist(pos, object) {
+  objectExist = (pos, object) => {
     return this.grid[pos].classList.contains(object);
-  }
+  };
 
   rotateDiv(pos, deg) {
     this.grid[pos].style.transform = `rotate(${deg}deg)`;
